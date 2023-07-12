4d7f66b0a3763cf620266c81b72b474b0bd2b201
nruxshod07 <nezametdinovruxsod@gmail.com>
2023-07-12 15:01:58 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,127 @@
+let students = [
+    {
+        id: Math.random(),
+        name: 'Timur',
+        info: {
+            school: '235',
+            faculity: 'SMM'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Imran',
+        info: {
+            school: 'ne izvestno',
+            faculity: 'programming'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Aminjon',
+        info: {
+            school: '444',
+            faculity: 'Dizayn'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Maxmud',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Muxammad',
+        info: {
+            school: '5555',
+            faculity: 'Backend'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Timur',
+        info: {
+            school: '235',
+            faculity: 'SMM'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Imran',
+        info: {
+            school: 'ne izvestno',
+            faculity: 'programming'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Aminjon',
+        info: {
+            school: '444',
+            faculity: 'Dizayn'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Maxmud',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Maxmud',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Muxammad',
+        info: {
+            school: '5555',
+            faculity: 'Backend'
+        },
+    },
+]
+
+let categories = [
+    {
+        course: ' SMM',
+        count: 0,
+        Names: []
+    },
+    {
+        course: 'PROGRAMMING',
+        count: 0,
+        Names: []
+    },
+    {
+        course: '     3DSMAX',
+        count: 0,
+        Names: []
+    },
+    {
+        course: ' DIZAYN',
+        count: 0,
+        Names: []
+    },
+    {
+        course: '   BACKEND',
+        count: 0,
+        Names: []
+    },
+]
+for (let student of students) {
+    for (let category of categories) {
+        if (student.info.faculity.toLowerCase().trim() === category.course.toLowerCase().trim()) {
+            category.Names.push(student)
+            category.count++
+        }
+    }
+}
+console.log(categories)
